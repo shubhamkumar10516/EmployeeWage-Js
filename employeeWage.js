0203c19663d3085f61dc816e4e02c82dc7e76a04
shubham <shubhamkumar10516@gmail.com>
2020-11-11 08:13:26 +0530

    [Shubham]ADDED: Employee wage calculate logic statement added

--- employeeWage.js
+++ employeeWage.js
@@ -10,3 +10,28 @@
     }
 }
 
+// Calculate Employee Wage
+const WAGE_PER_HOUR = 20 ; // Declaring as global for using wage calculator function anywhere
+{
+    const IS_PART_TIME = 1 ;
+    const IS_FULL_TIME = 2 ;
+    const PART_TIME_HOURS = 4 ;
+    const FULL_TIME_HOURS = 8 ;
+
+    function getWorkingHours(empCheck) {
+        switch (empCheck) {
+            case IS_PART_TIME :
+                return PART_TIME_HOURS ;
+            case IS_FULL_TIME : 
+                return FULL_TIME_HOURS ;
+            default :
+                return 0;
+        }
+    }
+    let empCheck = Math.floor(Math.random() * 10)%3;
+    let empHrs = getWorkingHours(empCheck);
+    let empWage = empHrs * WAGE_PER_HOUR ;
+    console.log("Employee Wage = " + empWage);
+}
+
+
